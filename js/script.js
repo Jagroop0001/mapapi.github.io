bb79cfaaf220d433352eab7d60eba7ddac387e30
Jagroop0001 <143255188+Jagroop0001@users.noreply.github.com>
2025-02-23 18:10:40 +0530

    chnaged to another map (leaflet)

--- js/script.js
+++ js/script.js
@@ -1,38 +1,33 @@
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+var map = L.map('map').setView([-0, 258.044], 3);
+// L.marker([-0, 258.044]).addTo(map)
+//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//     .openPopup();
 
-  const myLatlng = { lat: -1.363, lng: 258.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
-      center: myLatlng,
-  });
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-  let infoWindow = new google.maps.InfoWindow({
-      content: "Click anywhere to get automatic location",
-      position: myLatlng,
-  });
+map.on('click', function (e) {
+    var lat = e.latlng.lat;
+    var lng = e.latlng.lng;
+    document.getElementById("maplat").value = lat;
+    document.getElementById("maplon").value = lng;
+});
 
-  infoWindow.open(map);
-  map.addListener("click", (mapsMouseEvent) => {
-      const latLng = mapsMouseEvent.latLng.toJSON();
-      const lat = latLng.lat;
-      const lng = latLng.lng;
+/*
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault(); // Disable right-click
+});
 
-      document.getElementById("maplat").value = lat;
-      document.getElementById("maplon").value = lng;
-
-      // Close the current InfoWindow.
-      infoWindow.close();
-
-      // Create a new InfoWindow.
-      // infoWindow = new google.maps.InfoWindow({
-      //     position: mapsMouseEvent.latLng,
-      // });
-
-      // infoWindow.setContent(`Latitude: ${lat}<br>Longitude: ${lng}`);
-      // infoWindow.open(map);
-  });
-}
-initMap();
-
-
+document.addEventListener("keydown", function (e) {
+    if (
+        e.ctrlKey && (e.key === "u" || e.key === "U") || // Disable Ctrl+U (View Source)
+        e.ctrlKey && e.shiftKey && (e.key === "i" || e.key === "I") || // Disable Ctrl+Shift+I (DevTools)
+        e.ctrlKey && e.shiftKey && (e.key === "j" || e.key === "J") || // Disable Ctrl+Shift+J (Console)
+        e.ctrlKey && (e.key === "s" || e.key === "S") || // Disable Ctrl+S (Save Page)
+        e.key === "F12" // Disable F12 (DevTools)
+    ) {
+        e.preventDefault();
+    }
+});
+*/
