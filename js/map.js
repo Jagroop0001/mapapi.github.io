0acd1e446ef136ca4a9ed5150d0a6cde86a9c255
Jagroop0001 <143255188+Jagroop0001@users.noreply.github.com>
2025-03-29 03:36:37 +0530

    search feature added

--- js/map.js
+++ js/map.js
@@ -8,13 +8,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Listen for clicks on the map
+// Add Geosearch control
+const search = new window.GeoSearch.GeoSearchControl({
+    provider: new window.GeoSearch.OpenStreetMapProvider(),
+    style: 'bar',
+    showMarker: true,
+    showPopup: true,
+    keepResult: true,
+    autoClose: true,
+});
+
+map.addControl(search);
+
+// Listen for map clicks to update lat/lon fields
 map.on('click', function (event) {
     var lat = event.latlng.lat;
     var lng = event.latlng.lng;
 
-    // Display the latitude and longitude in an alert
-    //alert("Latitude: " + lat + "\nLongitude: " + lng);
     document.getElementById('maplat').value = lat;
     document.getElementById('maplon').value = lng;
 });
